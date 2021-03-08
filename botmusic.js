0933c58afa2843681c608d989bed935c818edd33
tiltadani <danielasilvarodrigues924@gmail.com>
2021-03-08 01:10:32 -0300

    Reprodução de músicas

--- botmusic.js
+++ botmusic.js
@@ -1,14 +1,24 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const ytdl = require('ytdl-core');
 
 require('dotenv').config();
 
-bot.on('message', msg => {
-    console.log(msg.content);
-
-    if (msg.content === "oi") {
-        msg.reply("Que foi mané?");
+bot.on('message', async message => {
+    // Voice only works in guilds, if the message does not come from a guild,
+    // we ignore it
+    if (!message.guild) return;
+  
+    if (message.content === '/join') {
+      // Only try to join the sender's voice channel if they are in one themselves
+      if (message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+        
+        connection.play(ytdl('https://youtu.be/8-Etf6dSjA4', { filter: 'audioonly' }));
+      } else {
+        message.reply('You need to join a voice channel first!');
+      }
     }
-})
+  });
 
 bot.login(process.env.TOKEN);
