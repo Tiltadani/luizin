bed1d7dff234218b1b9512c4150a15f2da9d318b
tiltadani <danielasilvarodrigues924@gmail.com>
2021-03-09 23:14:49 -0300

    mudanças

--- botmusic.js
+++ botmusic.js
@@ -1,24 +1,44 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const ytdl = require('ytdl-core');
+const streamOptions = {seek: 0, volume: 1};
 
 require('dotenv').config();
 
-bot.on('message', async message => {
-    // Voice only works in guilds, if the message does not come from a guild,
-    // we ignore it
-    if (!message.guild) return;
-  
-    if (message.content === '/join') {
-      // Only try to join the sender's voice channel if they are in one themselves
-      if (message.member.voice.channel) {
-        const connection = await message.member.voice.channel.join();
-        
-        connection.play(ytdl('https://youtu.be/8-Etf6dSjA4', { filter: 'audioonly' }));
-      } else {
-        message.reply('You need to join a voice channel first!');
-      }
-    }
-  });
-
 bot.login(process.env.TOKEN);
+
+bot.on('ready', () => {
+  console.log('Te amo doidinho! Te amo mais chata!');
+});
+
+bot.on('message', async msg => {
+  if (!message.guild) return;
+  if (msg.author.bot) return;
+  
+  if (msg.content.toLowerCase().startsWith('+play')) {
+    let VoiceChannel = msg.guild.channels.cache.find(channel => channel.id === '781668341953069087');
+    
+    console.log(VoiceChannel);
+
+    if (VoiceChannel == null) {
+      console.log('Canal não foi encontrado');
+    }
+
+    if (VoiceChannel !== null) {
+      console.log('O Canal foi encontrado');
+
+      VoiceChannel.join()
+        .then(connection => {
+          const stream = ytdl('https://youtu.be/-QdZ2VtOkhc', { filter: 'audioonly'});
+
+          const DJ = connection.play(stream, streamOptions);
+          
+          DJ.on('end', end => {
+            VoiceChannel.leave();
+          });
+        })
+        .catch(console.error);
+    }
+
+  }
+});
